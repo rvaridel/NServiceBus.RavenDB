87d515d75701997c30237bc43eb1a5167db4d44d
Rick Varidel <rvaridel@gmail.com>
2019-11-16 18:11:20 +1000

    Remove cluster topology check on initialisation

--- src/NServiceBus.RavenDB/Internal/DocumentStoreInitializer.cs
+++ src/NServiceBus.RavenDB/Internal/DocumentStoreInitializer.cs
@@ -28,7 +28,6 @@
                 ApplyConventions(settings);
 
                 docStore.Initialize();
-                EnsureClusterConfiguration(docStore);
             }
             isInitialized = true;
             return docStore;
@@ -59,23 +58,6 @@
                 if (usingDtc)
                 {
                     throw new Exception("RavenDB does not support Distributed Transaction Coordinator (DTC) transactions. You must change the TransportTransactionMode in order to continue. See the RavenDB Persistence documentation for more details.");
-                }
-            }
-        }
-
-        void EnsureClusterConfiguration(IDocumentStore store)
-        {
-            using (var s = store.OpenSession())
-            {
-                var getTopologyCmd = new GetClusterTopologyCommand();
-                s.Advanced.RequestExecutor.Execute(getTopologyCmd, s.Advanced.Context);
-
-                var topology = getTopologyCmd.Result.Topology;
-
-                // Currently do not support clusters with more than one possible primary member. Watchers (passive replication targets) are OK.
-                if (topology.Members.Count != 1)
-                {
-                    throw new InvalidOperationException("RavenDB Persistence does not support RavenDB clusters with more than one Leader/Member node. Only clusters with a single Leader and (optionally) Watcher nodes are supported.");
                 }
             }
         }
